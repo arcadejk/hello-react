0fc3079fe506b201559527971854ba44b232cdbd
Arcade Kakpo <arcadej@hotmail.fr>
2024-07-25 23:25:17 +0000

    Create composable elements breaking down the main code

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
-/**
-Challenge: 
-
-Part 1: Create a page of your own using a custom Page component
-
-It should return an ordered list with the reasons why you're
-excited to be learning React :)
-
-Render your list to the page
-
- */
-
 import React from "react"
 import ReactDOM from "react-dom"
 
+/**
+Challenge: 
+
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
+*/
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+        </div>
+    )
+}
+
 function Page() {
     return (
-        <ol>
-            <li>It's a popular library, so I'll be 
-            able to fit in with the cool kids!</li>
-            <li>I'm more likely to get a job as a developer
-            if I know React</li>
-        </ol>
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
     )
 }
 
