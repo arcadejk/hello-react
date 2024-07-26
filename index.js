f09136c3f42b652b7ec0f2e0bfcc70f7d3dfce74
Arcade Kakpo <arcadej@hotmail.fr>
2024-07-26 16:00:44 +0000

    Add main.js, navbar.js and css styles

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Header from "./Header"
-import Footer from "./Footer"
-import MainContent from "./MainContent"
+import App from "./App"
 
-
-function Page() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    )
-}
-
-ReactDOM.render(<Page />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
