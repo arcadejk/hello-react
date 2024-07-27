e150b752a31bdbb8d283ce8bf2f296c670298f02
Arcade Kakpo <arcadej@hotmail.fr>
2024-07-27 21:59:26 +0000

    Add main elements and css styles to HTML pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,3 @@
-/**
-Challenge: Complete the Navbar to match the design
-
-Hint: use the Figma file for the most accurate peek at the design
-(colors, sizes, fonts, etc.)
-*/
-
 import React from "react"
 
 export default function Navbar() {
